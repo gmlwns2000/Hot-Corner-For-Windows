3b660d93a09b07e6ed8bee85754cc313a75f32a2
gmlwns2000 <gmlwns5176@gmail.com>
2016-01-23 15:37:32 +0200

    init

---
+++ HotCorner/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace HotCorner
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte vk, byte scan, int flags, ref int extrainfo);
+         
+        static void Main(string[] args)
+        {
+            bool on = false;
+            while (true)
+            {
+                Thread.Sleep(1);
+                if(Cursor.Position.Y == 0 && Screen.PrimaryScreen.Bounds.Width - 1 == Cursor.Position.X)
+                {
+                    if (!on)
+                    {
+                        on = true;
+                        const byte VK_LWIN = 0x5B;
+                        const byte VK_TAB = 0x09;
+                        const int KEYUP = 0x02;
+                        int Info = 0;
+                        keybd_event(VK_LWIN, 0, 0, ref Info);
+                        keybd_event(VK_TAB, 0, 0, ref Info);
+                        keybd_event(VK_LWIN, 0, KEYUP, ref Info);
+                        keybd_event(VK_TAB, 0, KEYUP, ref Info);
+                    }
+                }
+                else
+                {
+                    if (on)
+                    {
+                        on = false;
+                    }
+                }
+            }
+        }
+    }
+}
